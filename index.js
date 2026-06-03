6a93b0bb7159a9a854e65a2da6a8609a88773c3a
Erik <kazumafan46@gmail.com>
2026-06-03 14:44:30 -0400

    Population

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const fs = require("fs");
 
+const articleData = {
+	mods: [
+		{
+			headerImage: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f7195997-7848-47de-bc04-bb31e278204f/dlqihiv-f469693c-1808-427f-a2fb-5b07ec7fd585.png/v1/fill/w_857,h_933,q_70,strp/alien_oc_again_by_redderblanket_dlqihiv-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTgwIiwicGF0aCI6Ii9mL2Y3MTk1OTk3LTc4NDgtNDdkZS1iYzA0LWJiMzFlMjc4MjA0Zi9kbHFpaGl2LWY0Njk2OTNjLTE4MDgtNDI3Zi1hMmZiLTViMDdlYzdmZDU4NS5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Y9AVEhMC87dzSdUCX5tnL5HzaIv72ZQoz9aSFZAzptE",
+			title: "Fat balatro cards",
+			description: "balatro balatrez"
+		}
+	],
+	projects: [
+		{
+			headerImage: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f7195997-7848-47de-bc04-bb31e278204f/dlqihiv-f469693c-1808-427f-a2fb-5b07ec7fd585.png/v1/fill/w_857,h_933,q_70,strp/alien_oc_again_by_redderblanket_dlqihiv-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTgwIiwicGF0aCI6Ii9mL2Y3MTk1OTk3LTc4NDgtNDdkZS1iYzA0LWJiMzFlMjc4MjA0Zi9kbHFpaGl2LWY0Njk2OTNjLTE4MDgtNDI3Zi1hMmZiLTViMDdlYzdmZDU4NS5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Y9AVEhMC87dzSdUCX5tnL5HzaIv72ZQoz9aSFZAzptE",
+			title: "Sci-fi world",
+			description: "my generic gooner sci-fi fantasy"
+		}
+	]
+};
+
 if (process.argv.includes("--insecure")) {
 
 	console.log("\nOpening on HTTP (--insecure)");
@@ -25,7 +42,23 @@ function onRequest(req, res) {
 		if (req.url == "/") {
 			
 			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-			res.end(fs.readFileSync("home.html", "utf-8"));
+
+			const articleCardTemplate = fs.readFileSync("article_card.htm", "utf-8");
+
+			let mods = "";
+			let projects = "";
+
+			for (const x of articleData.mods)
+				mods += articleCardTemplate.replace("insert_header.img", x.headerImage).replace("<!-- insert title -->", x.title).replace("<!-- insert description -->", x.description).replace("insert-download-is-flex-or-none", "flex");
+
+			for (const x of articleData.projects)
+				projects += articleCardTemplate.replace("insert_header.img", x.headerImage).replace("<!-- insert title -->", x.title).replace("<!-- insert description -->", x.description).replace("insert-download-is-flex-or-none", "none");
+
+			let page = fs.readFileSync("home.html", "utf-8");
+			page = page.replace("<!-- insert mods -->", mods);
+			page = page.replace("<!-- insert art projects -->", projects);
+
+			res.end(page);
 		
 		} else {
 			
