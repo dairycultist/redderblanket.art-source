b7ce8d0eb02832990c2c8f423a8a930e843ee0e6
Erik <kazumafan46@gmail.com>
2026-06-30 02:27:00 -0400

    Clean up php_server.js

--- php_server.js
+++ php_server.js
@@ -20,52 +20,42 @@ function on_request(req, res) {
 
 	console.log("\x1b[90m" + req.method + " " + req.url + "\x1b[0m");
 
-	if (req.method == "GET" || req.method == "HEAD") {
+	try {
+	
+		if (!req.method == "GET" && !req.method == "HEAD")
+			throw new Error("We only do GET and HEAD requests here");
 
 		let filepath = req.url.split("?", 2)[0];
 
 		if (req.url == "/")
 			filepath = "/index.php";
 
-		if (filepath.split(".").length == 2) { // no /img/../sensitive_file on us!
+		if (filepath.split(".").length != 2) // no /img/../sensitive_file on us!
+			throw new Error(filepath + " is not allowed!");
 
-			try {
+		let file;
 
-				let file;
+		if (filepath.split(".")[1] == "php") {
 
-				if (filepath.split(".")[1] == "php") {
+			file = process_php(fs.readFileSync("./home" + filepath, "utf-8"));
+			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 
-					file = process_php(fs.readFileSync("./home" + filepath, "utf-8"));
-					res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+		} else if (filepath.split(".")[1] == "png") {
 
-				} else if (filepath.split(".")[1] == "png") {
-
-					file = fs.readFileSync("./home" + filepath);
-					res.writeHead(200, { "Content-Type": "image/png" });
-
-				} else {
-
-					throw new Error("Unrecognized file type.");
-				}
-
-				res.end(file);
-
-			} catch (e) {
-				
-				res.writeHead(404, { "Content-Type": "text/plain" });
-				res.end("404 " + req.url + " Not Found; " + e);
-			}
+			file = fs.readFileSync("./home" + filepath);
+			res.writeHead(200, { "Content-Type": "image/png" });
 
 		} else {
-			
-			res.writeHead(403, { "Content-Type": "text/plain" });
-			res.end("403 " + req.url + " Is Not Allowed!");
+
+			throw new Error("Unrecognized file type.");
 		}
 
-	} else {
+		res.end(file);
 
-		res.writeHead(501, { "Content-Type": "text/plain; charset=utf-8" });
-		res.end("501 Not Implemented");
+	} catch (e) {
+
+		res.writeHead(400, { "Content-Type": "text/plain; charset=utf-8" });
+		res.end(e);
 	}
 }
 
