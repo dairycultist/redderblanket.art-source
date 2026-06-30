aa93370dd1e5d642751581d7dcba5bcde2a5dca6
Erik <kazumafan46@gmail.com>
2026-06-30 01:01:18 -0400

    I just want a website for my art

---
+++ test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+    <style>
+        body { background: #8fceff; margin: 0; }
+        figure { text-align: center; }
+        img { width: 100%; border: 4px outset #eee; }
+        figcaption { font-size: smaller; color: grey; }
+        main { margin: auto; background: white; width: 40em; min-height: 100vh; padding: 1em; }
+    </style>
+</head>
+<body>
+    <main>
+        <p>I'm a BBW/hyper artist. I want to make more sequences/short webcomics. Drawing makes me happy :)</p>
+        <p>I don't like my art's availability being at the whim of some megasite, so I'm soft-launching the migration to MY site that has zero SEO!</p>
+
+        <form style="text-align: center; background: #143b61; padding: 4px;">
+            <input type="text" name="keyword">
+            <input type="submit" value="Submit">
+        </form>
+        <div style="font-size: smaller; color: grey; text-align: center;">10 results</div>
+
+        <div style="display: grid; grid-template-columns: 1fr 1fr;">
+            <figure>
+                <img src="https://pbs.twimg.com/media/D1KyZsoX0AEPi67.jpg">
+                <figcaption>isabelle</figcaption>
+            </figure>
+        </div>
+    </main>
+</body>
+</html>
